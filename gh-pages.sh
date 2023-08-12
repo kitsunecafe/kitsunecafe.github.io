d4c1c08c1cd077cf30c6773ed47224dc83620f9d
Kitsune Cafe <10284516+kitsunecafe@users.noreply.github.com>
2023-08-12 04:40:03 -0400

    responsiveness tweaks; gh-pages fix

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,10 +3,7 @@
 if [ -f ".env" ]; then
 	source .env
 
-  
-	git add -f $DIST
-	git commit -m "updating gh-pages $(date)"
-	git push
+  git subtree push --prefix dist origin gh-pages
 	curl -u $GIT_USER:$GIT_TOKEN -X POST https://api.github.com/repos/$GIT_USER/$GIT_REPO/pages/builds
 fi
 
