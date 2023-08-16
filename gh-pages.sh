9330af45e95ce645bf18deccf796caeb35a5bca3
Kitsune Cafe <10284516+kitsunecafe@users.noreply.github.com>
2023-08-16 04:18:01 -0400

    update gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,9 +3,13 @@
 if [ -f ".env" ]; then
 	source .env
 
+  rm -rf "$DIST"
+  git worktree add "$DIST" "$GH_PAGES_BRANCH"
+  ./generate.sh
   (cd "$DIST"; git add -f .)
   (cd "$DIST"; git commit -m "Built from $(git log '--format=format:%H' main -1)")
   git push origin "$GH_PAGES_BRANCH"
+  git worktree remove "$DIST"
 fi
 
 
